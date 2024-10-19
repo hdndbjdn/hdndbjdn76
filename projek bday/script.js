e2bf84aa7bd14fdd350851691bf2aacdb039572d
hdndbjdn <hdndbjdn76@gmail.com>
2024-10-19 11:07:58 +0800

    Add files via upload

---
+++ projek bday/script.js
@@ -0,0 +1,30 @@
+let stars = document.getElementById('stars');
+let meteroid = document.getElementById('meteroid');
+let rocket = document.getElementById('rocket');
+let text = document.getElementById('text');
+let button = document.getElementById('button');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    rocket.style.top = value * -0.5 + 'px';
+    text.style.marginBottom = value * 1 + 'px';
+    button.style.marginBottom = value * 1 + 'px';
+})
+
+window.addEventListener('load', () => {
+    Swal.fire({
+        title: 'Do you want to play music in the background?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.music').play();
+        }
+            AnimationTimeline()
+    })
+})
